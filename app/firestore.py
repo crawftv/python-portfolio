93f1ff822da3c97d6aa3b5cb3724f16d6d41ef17
crawford <crawford.collins@gmail.com>
2019-04-29 00:22:28 -0500

    add a function to put gihub repo info in firestore

--- app/firestore.py
+++ app/firestore.py
@@ -5,6 +5,7 @@ from decouple import config
 from firebase_admin import firestore
 import requests
 import itertools
+
 cred = credentials.Certificate(config("FIRESTORE_JSON"))
 firebase_admin.initialize_app(cred,{
     'projectID' : 'porftfolio-a7d76' 
@@ -17,13 +18,14 @@ def get_medium(db):
     blog_list = [ doc.to_dict() for doc in docs]
     return blog_list
 
-def update_github_events(page_num):
+def update_github_events(db, page_num):
     r = requests.get('https://api.github.com/users/'+config("GITHUB_USERNAME")+
             '/events?page='+str(page_num))
     query = r.json()
     for q in query:
         data = { u'gheID': q['id'], u'gheTime': q['created_at'][0:10] }
         db.collection(u'github-events').document(q['id']).set(data)
+
 def get_github_events(db):
     github_events = db.collection('github-events').get()
     github_events = [ event.to_dict() for event in github_events ]
@@ -33,4 +35,11 @@ def get_github_events(db):
     data   = [ len(github_events[i][1]) for i in range(len(github_events)) ]
     labels = json.dumps(labels)
     data = json.dumps(data)
-    return labels,data
+    return labels, data
+
+def update_repos(db,page_num):
+    r = requests.get('https://api.github.com/users/'+config("GITHUB_USERNAME")+"/repos?page="+str(page_num))
+    query = r.json()
+    for q in query:
+        data = {u'repo_name':q['name'], u'repo_id':q["id"] }
+        db.collection(u'git-repos').document(str(q['name'])).set(data)
