870ca9eea5ca17684b3f8327c9fbb3f8a4227283
crawford <crawford.collins@gmail.com>
2019-05-22 17:01:35 -0500

    fixed github chart, blackened .py files

--- application.py
+++ application.py
@@ -5,32 +5,39 @@ from firestore_utilities import get_medium, db, get_github_events, get_projects
 application = Flask(__name__)
 
 
-@application.route('/')
+@application.route("/")
 def root():
-    return render_template('root2.html')
+    return render_template("root2.html")
 
-@application.route('/blog')
+
+@application.route("/blog")
 def blog():
     blog_list = get_medium(db)
-    return render_template("blog.html",blog_list = blog_list)
+    return render_template("blog.html", blog_list=blog_list)
 
-@application.route('/github')
+
+@application.route("/github")
 def github():
-    labels, data = get_github_events(db)
-    return render_template("github.html",labels=labels, data=data)
+    labels, data, num_events = get_github_events(db)
+    return render_template(
+        "github.html", labels=labels, data=data, num_events=num_events
+    )
 
-@application.route('/projects')
+
+@application.route("/projects")
 def projects():
     projects = get_projects(db)
-    return render_template('projects.html', projects = projects)
+    return render_template("projects.html", projects=projects)
 
-@application.route('/twitter')
+
+@application.route("/twitter")
 def twitter():
-    return render_template('twitter.html')
+    return render_template("twitter.html")
+
 
 # @application.route('/test')
 # def test():
 #     return render_template("root2.html")
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    application.run(host="0.0.0.0")
--- firestore_utilities.py
+++ firestore_utilities.py
@@ -9,76 +9,112 @@ import re
 import time
 
 cred = credentials.Certificate(config("FIRESTORE_JSON"))
-firebase_admin.initialize_app(cred,{
-    'projectID' : 'porftfolio-a7d76' 
-    })
+firebase_admin.initialize_app(cred, {"projectID": "porftfolio-a7d76"})
 
 db = firestore.client()
 
+
 def get_medium(db):
-    docs = db.collection('medium-stories').order_by(u'publish_num',
-            direction=firestore.Query.DESCENDING).get()
-    blog_list = [ doc.to_dict() for doc in docs]
+    docs = (
+        db.collection("medium-stories")
+        .order_by(u"publish_num", direction=firestore.Query.DESCENDING)
+        .get()
+    )
+    blog_list = [doc.to_dict() for doc in docs]
     return blog_list
 
+
 def get_projects(db):
-    projects = db.collection('projects').order_by('class',
-            direction=firestore.Query.ASCENDING).get()
+    projects = (
+        db.collection("projects")
+        .order_by("class", direction=firestore.Query.ASCENDING)
+        .get()
+    )
     projects = [doc.to_dict() for doc in projects]
     return projects
 
 
-
 def update_github_events(db, page_num):
-    r = requests.get('https://api.github.com/users/'+config("GITHUB_USERNAME")+
-            '/events?page='+str(page_num))
+    r = requests.get(
+        "https://api.github.com/users/"
+        + config("GITHUB_USERNAME")
+        + "/events?page="
+        + str(page_num)
+    )
     query = r.json()
     for q in query:
-        data = { u'gheID': q['id'], u'gheTime': q['created_at'][0:10] }
-        db.collection(u'github-events').document(q['id']).set(data)
+        data = {u"gheID": q["id"], u"gheTime": q["created_at"][0:10]}
+        db.collection(u"github-events").document(q["id"]).set(data)
+
 
 def get_github_events(db):
-    github_events = db.collection('github-events').get()
-    github_events = [ event.to_dict() for event in github_events ]
-    github_events = [ (key, list(num for _, num in value)) for key, value in
-            itertools.groupby(github_events, lambda x:x['gheTime']) ]
-    labels = [ github_events[i][0] for i in range(len(github_events)) ]
-    data   = [ len(github_events[i][1]) for i in range(len(github_events)) ]
+    github_events = db.collection("github-events").get()
+    github_events = [event.to_dict() for event in github_events]
+    num_events = len(github_events)
+    github_events = [
+        (key, list(num for _, num in value))
+        for key, value in itertools.groupby(github_events, lambda x: x["gheTime"])
+    ]
+    labels = [github_events[i][0] for i in range(len(github_events))]
+    data = [len(github_events[i][1]) for i in range(len(github_events))]
     labels = json.dumps(labels)
     data = json.dumps(data)
-    return labels, data
+    return labels, data, num_events
 
-def update_repos(db,page_num):
-    r = requests.get('https://api.github.com/users/'+config("GITHUB_USERNAME")+"/repos?page="+str(page_num))
+
+def update_repos(db, page_num):
+    r = requests.get(
+        "https://api.github.com/users/"
+        + config("GITHUB_USERNAME")
+        + "/repos?page="
+        + str(page_num)
+    )
     query = r.json()
     for q in query:
-        data = {u'repo_name':q['name'], u'repo_id':q["id"] }
-        db.collection(u'git-repos').document(str(q['name'])).set(data)
+        data = {u"repo_name": q["name"], u"repo_id": q["id"]}
+        db.collection(u"git-repos").document(str(q["name"])).set(data)
+
 
 def update_repo_files(db):
-    repos = db.collection('git-repos').get()
-    repos = [ repo.to_dict() for repo in repos ]
+    repos = db.collection("git-repos").get()
+    repos = [repo.to_dict() for repo in repos]
     for repo in repos:
         time.sleep(120)
-        sha = requests.get('https://api.github.com/repos/'+config("GITHUB_USERNAME")+"/"+repo["repo_name"]+"/commits")
+        sha = requests.get(
+            "https://api.github.com/repos/"
+            + config("GITHUB_USERNAME")
+            + "/"
+            + repo["repo_name"]
+            + "/commits"
+        )
         sha = sha.json()[0]["sha"]
         try:
-            tree = requests.get('https://api.github.com/repos/'+config("GITHUB_USERNAME")+"/"+repo["repo_name"]+
-                        '/git/trees/'+sha+'?recursive=1').json()
+            tree = requests.get(
+                "https://api.github.com/repos/"
+                + config("GITHUB_USERNAME")
+                + "/"
+                + repo["repo_name"]
+                + "/git/trees/"
+                + sha
+                + "?recursive=1"
+            ).json()
 
-            tree = [ t["path"] for t in tree["tree"] ]
+            tree = [t["path"] for t in tree["tree"]]
             tree = check_file_type(tree)
-            db.collection(u'git-repos').document(str(repo["repo_name"])).set({u'tree': tree }, merge=True)
+            db.collection(u"git-repos").document(str(repo["repo_name"])).set(
+                {u"tree": tree}, merge=True
+            )
             print(repo["repo_name"])
         except:
             pass
 
-#UTILITY FUNCTIONS BELOW
+
+# UTILITY FUNCTIONS BELOW
 def check_file_type(list_string):
     new_list = []
     for s in list_string:
-        if re.match(r'^.*(node_modules|solutions|elm-stuff)',s):
+        if re.match(r"^.*(node_modules|solutions|elm-stuff)", s):
             pass
-        elif re.match(r'^.*\.(py|js|jl|elm|ipynb|html|ts|sh|php)',s):
+        elif re.match(r"^.*\.(py|js|jl|elm|ipynb|html|ts|sh|php)", s):
             new_list.append(s)
     return new_list
